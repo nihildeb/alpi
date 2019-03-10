5f078a806a0f1b7d19f62c6f121abdf0aafa3839
root <root@rpi.fritz.box>
2019-03-10 21:53:16 +0000

    fuckin a

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -6,15 +6,6 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-# git
-alias ga='git add .'
-alias gs='git status'
-alias gp='git push'
-alias gca='git commit -v -a'
-alias gl='git pull'
-alias gb='git branch'
-alias gco='git checkout'
-
 # xplatform shell helpers
 alias flush='dscacheutil -flushcache'
 alias cls='clear'
@@ -30,12 +21,6 @@ alias update='wget -q -O - http://test.ipsaw.com:8080/install |sh'
 alias updater='update && lbusave && reboot'
 alias reboot='killall squid; reboot;'
 
-alias squid-start='rc-service squid start'
-alias squid-enable='rc-update add squid'
-alias squid-reconfigure='squid -k reconfigure'
-alias squid-check='squid -k check'
-alias squid-stat='netstat -tl'
-
 function nginxinit() {
   adduser -D -g 'www' www
   mkdir /www
@@ -45,5 +30,11 @@ function nginxinit() {
 alias nginx-init='nginxinit'
 alias nginx-start='rc-service nginx start'
 alias nginx-enable='rc-update add nginx'
+
+alias squid-start='rc-service squid start'
+alias squid-enable='rc-update add squid'
+alias squid-reconfigure='squid -k reconfigure'
+alias squid-check='squid -k check'
+alias squid-stat='netstat -tl'
 
 
