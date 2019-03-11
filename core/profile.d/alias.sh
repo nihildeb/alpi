75a59f650da8dfdd949b7792161ebb11430c5513
Nick.Hildebrant <nick.hildebrant@gmail.com>
2019-03-11 13:24:37 +0100

    aliases as shell scripts

--- core/profile.d/alias.sh
+++ core/profile.d/alias.sh
@@ -1,48 +1,21 @@
 #!/bin/sh
-# Aliases from http://test.ipsaw.com:8080/install
-
-# some ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-# xplatform shell helpers
-alias flush='dscacheutil -flushcache'
-alias cls='clear'
-alias h='history'
 alias ..='cd ..'
 alias ...='cd ../..'
 
-# git
-alias ga='git add .'
-#alias gs='git status'
-alias gp='git push'
-alias gca='git commit -v -a'
-alias gl='git pull'
-alias gb='git branch'
-alias gco='git checkout'
+#function nginxinit() {
+  #adduser -D -g 'www' www
+  #mkdir /www
+  #chown -R www:www /var/lib/nginx
+  #chown -R www:www /www
+#}
+#alias nginx-init='nginxinit'
+#alias nginx-start='rc-service nginx start'
+#alias nginx-enable='rc-update add nginx'
 
-# alpi
-alias lbusave='lbu ci -v -d'
-alias lbusavedry='lbu ci -n -v'
-alias lbustat='lbu status'
-alias update='wget -q -O - https://raw.githubusercontent.com/nihildeb/alpi/master/install |sh'
-alias updater='update && lbusave && reboot'
-
-function nginxinit() {
-  adduser -D -g 'www' www
-  mkdir /www
-  chown -R www:www /var/lib/nginx
-  chown -R www:www /www
-}
-alias nginx-init='nginxinit'
-alias nginx-start='rc-service nginx start'
-alias nginx-enable='rc-update add nginx'
-
-alias squid-start='rc-service squid start'
-alias squid-enable='rc-update add squid'
-alias squid-reconfigure='squid -k reconfigure'
-alias squid-check='squid -k check'
-alias squid-stat='netstat -tl'
+#alias squid-start='rc-service squid start'
+#alias squid-enable='rc-update add squid'
+#alias squid-reconfigure='squid -k reconfigure'
+#alias squid-check='squid -k check'
+#alias squid-stat='netstat -tl'
 
 
